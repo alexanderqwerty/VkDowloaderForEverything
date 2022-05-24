60de164b54362b466d38acdaa4a1fa536d2c4048
Alex <duducensash@gmail.com>
2022-05-24 10:14:07 +0300

    delete useless function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,8 +9,10 @@ import com.vk.api.sdk.exceptions.ClientException;
 import com.vk.api.sdk.httpclient.HttpTransportClient;
 import com.vk.api.sdk.objects.GroupAuthResponse;
 import com.vk.api.sdk.objects.UserAuthResponse;
+import com.vk.api.sdk.objects.Validable;
 import com.vk.api.sdk.objects.messages.HistoryAttachment;
 import com.vk.api.sdk.objects.messages.Message;
+import com.vk.api.sdk.objects.messages.MessageAttachment;
 import com.vk.api.sdk.queries.messages.MessagesGetHistoryAttachmentsQuery;
 import com.vk.api.sdk.queries.messages.MessagesGetLongPollHistoryQuery;
 import org.apache.poi.ss.formula.atp.Switch;
@@ -37,76 +39,93 @@ public class Main {
     //static String fuck =
     static Integer groupid = 213452188;
 
+    public static void getPhoto(MessageAttachment attachment) throws IOException {
+        System.out.println(attachment.getPhoto().getSizes().get(attachment.getPhoto().getSizes().size() - 1).getUrl());
+        URL url = new URL(attachment.getPhoto().getSizes().get(attachment.getPhoto().getSizes().size() - 1).getUrl().toString());
+        Files.copy(url.openStream(), Paths.get("C:\\Users\\duduc\\OneDrive\\Рабочий стол\\" + attachment.getPhoto().getId() + ".jpeg"), StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    public static void getAudio(MessageAttachment attachment) throws IOException {
+        System.out.println(attachment.getAudio().getUrl());
+        URL url = new URL(attachment.getAudio().getUrl().toString());
+        Files.copy(url.openStream(), Paths.get("C:\\Users\\duduc\\OneDrive\\Рабочий стол\\" + attachment.getAudio().getTitle() + ".mp3"), StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    public static void getDoc(MessageAttachment attachment) throws IOException {
+        System.out.println(attachment.getDoc().getUrl());
+        URL url = new URL(attachment.getDoc().getUrl().toString());
+        Files.copy(url.openStream(), Paths.get("C:\\Users\\duduc\\OneDrive\\Рабочий стол\\" + attachment.getDoc().getTitle()), StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    public static void getGraffiti(MessageAttachment attachment) throws IOException {
+        System.out.println(attachment.getGraffiti().getUrl());
+        URL url = new URL(attachment.getGraffiti().getUrl().toString());
+        Files.copy(url.openStream(), Paths.get("C:\\Users\\duduc\\OneDrive\\Рабочий стол\\" + attachment.getGraffiti().getId() + ".png"), StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    public static void getAudioMessage(MessageAttachment attachment) throws IOException {
+        System.out.println(attachment.getAudioMessage().getLinkMp3());
+        URL url = new URL(attachment.getAudioMessage().getLinkMp3().toString());
+        Files.copy(url.openStream(), Paths.get("C:\\Users\\duduc\\OneDrive\\Рабочий стол\\" + attachment.getAudioMessage().getId() + ".mp3"), StandardCopyOption.REPLACE_EXISTING);
+    }
+
     public static void main(String[] args) throws ClientException, ApiException, InterruptedException {
         TransportClient transportClient = new HttpTransportClient();
         VkApiClient vk = new VkApiClient(transportClient);
-
+        Random random = new Random();
         GroupActor a = new GroupActor(groupid, accessToken);
         System.out.println(vk.messages().getLongPollServer(a).execute().toString());
         Integer ts = vk.messages().getLongPollServer(a).execute().getTs();
-        Random random = new Random();
+
         while (true) {
             MessagesGetLongPollHistoryQuery historyQuery = vk.messages().getLongPollHistory(a).ts(ts);
-            MessagesGetHistoryAttachmentsQuery attachmentsQuery = vk.messages().getHistoryAttachments(a, -groupid);
-
             List<Message> messages = historyQuery.execute().getMessages().getItems();
 
             if (!messages.isEmpty()) {
                 messages.forEach(message -> {
-                    message.getAttachments().forEach(attachment -> {
-                        URL url = null;
-                        try {
-                            switch (attachment.getType()) {
-                                case PHOTO -> {
-                                    System.out.println(attachment.getPhoto().getSizes().get(attachment.getPhoto().getSizes().size() - 1).getUrl());
-                                    url = new URL(attachment.getPhoto().getSizes().get(attachment.getPhoto().getSizes().size() - 1).getUrl().toString());
-                                    Files.copy(url.openStream(), Paths.get("C:\\Users\\duduc\\OneDrive\\Рабочий стол\\" + attachment.getPhoto().getId() + ".jpeg"), StandardCopyOption.REPLACE_EXISTING);
-
-                                }
-                                case AUDIO -> {
-                                    System.out.println(attachment.getAudio().getUrl());
-                                    url = new URL(attachment.getAudio().getUrl().toString());
-                                    Files.copy(url.openStream(), Paths.get("C:\\Users\\duduc\\OneDrive\\Рабочий стол\\" + attachment.getAudio().getTitle() + ".mp3"), StandardCopyOption.REPLACE_EXISTING);
-
-                                }
-                                case VIDEO -> {
+                    if (!message.getAttachments().isEmpty())
+                        message.getAttachments().forEach(attachment -> {
+                                    try {
+                                        switch (attachment.getType()) {
+                                            case PHOTO -> getPhoto(attachment);
+                                            case AUDIO -> getAudio(attachment);
+                                            case VIDEO -> {
 //                                    System.out.println(attachment.getVideo());
 //                                    url = new URL("https://vk.com/video_ext.php?oid=390073364&id=456239168&hash=1cd119acb795bc2f");
 //                                    Files.copy(url.openStream(),Paths.get("C:\\Users\\duduc\\OneDrive\\Рабочий стол\\" + "mp3.mp4"),StandardCopyOption.REPLACE_EXISTING);
+                                            }
+                                            case DOC -> getDoc(attachment);
+                                            case GRAFFITI -> getGraffiti(attachment);
+                                            case AUDIO_MESSAGE -> getAudioMessage(attachment);
+                                            default -> vk.messages().send(a).userId(message.getId()).randomId(random.nextInt(10000)).message("Пока не поддерживается").execute();
+                                        }
+                                    } catch (IOException | ApiException | ClientException e) {
 
-                                }
-                                case DOC -> {
-                                    System.out.println(attachment.getDoc().getUrl());
-                                    url = new URL(attachment.getDoc().getUrl().toString());
-                                    Files.copy(url.openStream(), Paths.get("C:\\Users\\duduc\\OneDrive\\Рабочий стол\\" + attachment.getDoc().getTitle()), StandardCopyOption.REPLACE_EXISTING);
-                                }
+                                    }
 
-                                case GRAFFITI -> {
-                                    System.out.println(attachment.getGraffiti().getUrl());
-                                    url = new URL(attachment.getGraffiti().getUrl().toString());
-                                    Files.copy(url.openStream(), Paths.get("C:\\Users\\duduc\\OneDrive\\Рабочий стол\\" + attachment.getGraffiti().getId() + ".png"), StandardCopyOption.REPLACE_EXISTING);
+                        });
+                    else if(!message.getReplyMessage().getAttachments().isEmpty()) {
+                        message.getReplyMessage().getAttachments().forEach(attachment -> {
+                            try {
+                                switch (attachment.getType()) {
+                                    case PHOTO -> getPhoto(attachment);
+                                    case AUDIO -> getAudio(attachment);
+                                    case VIDEO -> {
+//                                    System.out.println(attachment.getVideo());
+//                                    url = new URL("https://vk.com/video_ext.php?oid=390073364&id=456239168&hash=1cd119acb795bc2f");
+//                                    Files.copy(url.openStream(),Paths.get("C:\\Users\\duduc\\OneDrive\\Рабочий стол\\" + "mp3.mp4"),StandardCopyOption.REPLACE_EXISTING);
+                                    }
+                                    case DOC -> getDoc(attachment);
+                                    case GRAFFITI -> getGraffiti(attachment);
+                                    case AUDIO_MESSAGE -> getAudioMessage(attachment);
+                                    default -> vk.messages().send(a).userId(message.getId()).randomId(random.nextInt(10000)).message("Пока не поддерживается").execute();
                                 }
-                                case AUDIO_MESSAGE -> {
-                                    System.out.println(attachment.getAudioMessage().getLinkMp3());
-                                    url = new URL(attachment.getAudioMessage().getLinkMp3().toString());
-                                    Files.copy(url.openStream(), Paths.get("C:\\Users\\duduc\\OneDrive\\Рабочий стол\\" + attachment.getAudioMessage().getId() + ".mp3"), StandardCopyOption.REPLACE_EXISTING);
-                                }
-                                default -> {
-                                    vk.messages().send(a).userId(message.getFromId()).randomId(random.nextInt(10000)).message("Пока не поддерживается").execute();
-                                }
+                            } catch (IOException | ApiException | ClientException e) {
+
                             }
-                        } catch (IOException | ApiException | ClientException e) {
 
-                        }
-                    });
-//                        URL url = new URL(message.getAttachments().get(message.getAttachments().size() - 1).getDoc().getUrl().toString());
-//                        InputStream in = url.openStream();
-//                        message.getAttachments().forEach(attachment -> {
-//                            System.out.println(attachment.getDoc().getTitle());
-//                            vk.messages().send(a).message(attachment.getDoc().getTitle() + " Скачался").randomId(random.nextInt(10000)).userId(message.getFromId()).execute();
-//                            Files.copy(in, Paths.get("C:\\Users\\duduc\\OneDrive\\Рабочий стол\\" + attachment.getDoc().getTitle()), StandardCopyOption.REPLACE_EXISTING);
-//                        });
-
+                        });
+                    }
                 });
             }
             ts = vk.messages().getLongPollServer(a).execute().getTs();
