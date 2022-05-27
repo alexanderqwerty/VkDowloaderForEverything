b3e34466430606ef797f9091662c476a9a6919d1
Alex <duducensash@gmail.com>
2022-05-27 22:41:17 +0300

    pretty code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -36,8 +36,11 @@ import java.util.Random;
 
 public class Main {
     static String accessToken = "4da688544537d33cd6f120a4cef3e678b84074bae59038ed9729db382f697395c418802676078551790e7";
-    //static String fuck =
+    static String fuck ="Иди нахуй ваня";
+    static String director= "F:\\";
     static Integer groupid = 213452188;
+    static Integer ivan = 397698080;
+    static Integer my = 397698080;
 
     public static void getPhoto(MessageAttachment attachment) throws IOException {
         System.out.println(attachment.getPhoto().getSizes().get(attachment.getPhoto().getSizes().size() - 1).getUrl());
@@ -45,28 +48,35 @@ public class Main {
         Files.copy(url.openStream(), Paths.get("C:\\Users\\duduc\\OneDrive\\Рабочий стол\\" + attachment.getPhoto().getId() + ".jpeg"), StandardCopyOption.REPLACE_EXISTING);
     }
 
+    public static void getVideo(VkApiClient vk, GroupActor a) throws IOException, ClientException, ApiException {
+        vk.messages().send(a)
+                .message("Не поддерживается скачивание видео")
+                .randomId(new Random().nextInt(10000))
+                .execute();
+    }
+
     public static void getAudio(MessageAttachment attachment) throws IOException {
         System.out.println(attachment.getAudio().getUrl());
         URL url = new URL(attachment.getAudio().getUrl().toString());
-        Files.copy(url.openStream(), Paths.get("C:\\Users\\duduc\\OneDrive\\Рабочий стол\\" + attachment.getAudio().getTitle() + ".mp3"), StandardCopyOption.REPLACE_EXISTING);
+        Files.copy(url.openStream(), Paths.get(director + attachment.getAudio().getTitle() + ".mp3"), StandardCopyOption.REPLACE_EXISTING);
     }
 
     public static void getDoc(MessageAttachment attachment) throws IOException {
         System.out.println(attachment.getDoc().getUrl());
         URL url = new URL(attachment.getDoc().getUrl().toString());
-        Files.copy(url.openStream(), Paths.get("C:\\Users\\duduc\\OneDrive\\Рабочий стол\\" + attachment.getDoc().getTitle()), StandardCopyOption.REPLACE_EXISTING);
+        Files.copy(url.openStream(), Paths.get(director + attachment.getDoc().getTitle()), StandardCopyOption.REPLACE_EXISTING);
     }
 
     public static void getGraffiti(MessageAttachment attachment) throws IOException {
         System.out.println(attachment.getGraffiti().getUrl());
         URL url = new URL(attachment.getGraffiti().getUrl().toString());
-        Files.copy(url.openStream(), Paths.get("C:\\Users\\duduc\\OneDrive\\Рабочий стол\\" + attachment.getGraffiti().getId() + ".png"), StandardCopyOption.REPLACE_EXISTING);
+        Files.copy(url.openStream(), Paths.get(director + attachment.getGraffiti().getId() + ".png"), StandardCopyOption.REPLACE_EXISTING);
     }
 
     public static void getAudioMessage(MessageAttachment attachment) throws IOException {
         System.out.println(attachment.getAudioMessage().getLinkMp3());
         URL url = new URL(attachment.getAudioMessage().getLinkMp3().toString());
-        Files.copy(url.openStream(), Paths.get("C:\\Users\\duduc\\OneDrive\\Рабочий стол\\" + attachment.getAudioMessage().getId() + ".mp3"), StandardCopyOption.REPLACE_EXISTING);
+        Files.copy(url.openStream(), Paths.get(director + attachment.getAudioMessage().getId() + ".mp3"), StandardCopyOption.REPLACE_EXISTING);
     }
 
     public static void main(String[] args) throws ClientException, ApiException, InterruptedException {
@@ -80,45 +90,42 @@ public class Main {
         while (true) {
             MessagesGetLongPollHistoryQuery historyQuery = vk.messages().getLongPollHistory(a).ts(ts);
             List<Message> messages = historyQuery.execute().getMessages().getItems();
-
             if (!messages.isEmpty()) {
                 messages.forEach(message -> {
                     if (!message.getAttachments().isEmpty())
                         message.getAttachments().forEach(attachment -> {
-                                    try {
-                                        switch (attachment.getType()) {
-                                            case PHOTO -> getPhoto(attachment);
-                                            case AUDIO -> getAudio(attachment);
-                                            case VIDEO -> {
-//                                    System.out.println(attachment.getVideo());
-//                                    url = new URL("https://vk.com/video_ext.php?oid=390073364&id=456239168&hash=1cd119acb795bc2f");
-//                                    Files.copy(url.openStream(),Paths.get("C:\\Users\\duduc\\OneDrive\\Рабочий стол\\" + "mp3.mp4"),StandardCopyOption.REPLACE_EXISTING);
-                                            }
-                                            case DOC -> getDoc(attachment);
-                                            case GRAFFITI -> getGraffiti(attachment);
-                                            case AUDIO_MESSAGE -> getAudioMessage(attachment);
-                                            default -> vk.messages().send(a).userId(message.getId()).randomId(random.nextInt(10000)).message("Пока не поддерживается").execute();
-                                        }
-                                    } catch (IOException | ApiException | ClientException e) {
+                            try {
+                                switch (attachment.getType()) {
+                                    case PHOTO -> getPhoto(attachment);
+                                    case AUDIO -> getAudio(attachment);
+                                    case VIDEO -> getVideo(vk, a);
+                                    case DOC -> getDoc(attachment);
+                                    case GRAFFITI -> getGraffiti(attachment);
+                                    case AUDIO_MESSAGE -> getAudioMessage(attachment);
+                                    default -> vk.messages().send(a).userId(message.getId())
+                                            .randomId(random.nextInt(10000))
+                                            .message("Пока не поддерживается")
+                                            .execute();
+                                }
+                            } catch (IOException | ApiException | ClientException e) {
 
-                                    }
+                            }
 
                         });
-                    else if(!message.getReplyMessage().getAttachments().isEmpty()) {
+                    else if (!message.getReplyMessage().getAttachments().isEmpty()) {
                         message.getReplyMessage().getAttachments().forEach(attachment -> {
                             try {
                                 switch (attachment.getType()) {
                                     case PHOTO -> getPhoto(attachment);
                                     case AUDIO -> getAudio(attachment);
-                                    case VIDEO -> {
-//                                    System.out.println(attachment.getVideo());
-//                                    url = new URL("https://vk.com/video_ext.php?oid=390073364&id=456239168&hash=1cd119acb795bc2f");
-//                                    Files.copy(url.openStream(),Paths.get("C:\\Users\\duduc\\OneDrive\\Рабочий стол\\" + "mp3.mp4"),StandardCopyOption.REPLACE_EXISTING);
-                                    }
+                                    case VIDEO -> getVideo(vk,a);
                                     case DOC -> getDoc(attachment);
                                     case GRAFFITI -> getGraffiti(attachment);
                                     case AUDIO_MESSAGE -> getAudioMessage(attachment);
-                                    default -> vk.messages().send(a).userId(message.getId()).randomId(random.nextInt(10000)).message("Пока не поддерживается").execute();
+                                    default -> vk.messages().send(a).userId(message.getId())
+                                            .randomId(random.nextInt(10000))
+                                            .message("Пока не поддерживается")
+                                            .execute();
                                 }
                             } catch (IOException | ApiException | ClientException e) {
 
